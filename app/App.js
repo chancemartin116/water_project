887e2fa9080fec762d58da251e28e2bdcd2bc7f0
212449799 <chance.martin@ge.com>
2017-04-02 13:53:43 -0700

    new repo

---
+++ app/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import classes from './App.scss';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <img className="logo" src="SWIM.png" alt="SWIM"/>
+        <div className="menuBar"></div>
+        <ul>
+          <li>
+            <a>Water Quality</a>
+          </li>
+          <li>Climate, Hydrology & Climate Change  Resiliency</li>
+          <li>Water Management Planning</li>
+          <li>Infrastructure</li>
+          <li>Biological/Ecological Information</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export const mapStateToProps = (state) => {
+  return {
+  };
+};
+
+export const mapDispatchToProps = (dispatch) => {
+  return {
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+
+// TODO: add interactive map for each page, add page for each bullet, pics of community center and exhibits, link for teachers: lesson plans, etc., literature database,
+// TODO: run analytics on map
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config.js');
+const app = express();
+ 
+const compiler = webpack(webpackConfig);
+ 
+app.use(express.static(__dirname + '/public'));
+ 
+app.use(webpackDevMiddleware(compiler, {
+  hot: true,
+  filename: 'bundle.js',
+  publicPath: '/',
+  stats: {
+    colors: true,
+  },
+  historyApiFallback: true,
+}));
+
+var port = process.env.PORT ? process.env.PORT : 3000;
+ 
+const server = app.listen(port, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
